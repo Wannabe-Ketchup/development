c3d1a553f2ed9fc7f45959569abbe41c4d2d6ab0
한지은 <hjn510450@gmail.com>
2026-09-03 13:37:00 +0900

    feat: SSE 통신용 공통 이벤트 타입 정의 (#16)

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,3 +3,4 @@ export * from './constants/message.js';
 export * from './types/timer.js';
 export * from './types/room.js';
 export * from './types/participant.js';
+export * from './types/sse-event.js';
