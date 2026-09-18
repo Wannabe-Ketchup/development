e980a53f9ef61cca4e5f2fa74b1b3590b7737b86
young999999999 <ssas146310@gmail.com>
2026-09-18 23:30:41 +0900

    chore: [리뷰반영] 리버스 프록시 신뢰 설정 추가

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
+
+  app.set('trust proxy', 1);
 
   // dev 환경이 아닐 때 NestJS 기본 로거를 winston으로 설정
   if (configService.get('NODE_ENV') !== 'dev') {
