656c416d47f5ebd5640e9076a571bdad0cee26f0
young999999999 <ssas146310@gmail.com>
2026-09-18 17:14:32 +0900

    feat: 환경별 Winston 로거 모듈 구현 및 main.ts 적용

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // dev 환경이 아닐 때 NestJS 기본 로거를 winston으로 설정
+  if (process.env.NODE_ENV !== 'dev') {
+    app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
