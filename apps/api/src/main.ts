5991472f00595031e39f1f3240ba671d1b379f5e
young999999999 <ssas146310@gmail.com>
2026-09-18 23:16:15 +0900

    chore: [리뷰반영] 환경 변수를 nest config로 일관되게 받도록 수정

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   // dev 환경이 아닐 때 NestJS 기본 로거를 winston으로 설정
-  if (process.env.NODE_ENV !== 'dev') {
+  if (configService.get('NODE_ENV') !== 'dev') {
     app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   }
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(configService.get('PORT') ?? 3000);
 }
 void bootstrap();
