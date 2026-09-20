5851676b481d00dccc59c9b3bf55b331691bc0fc
한지은 <hjn510450@gmail.com>
2026-09-21 00:34:25 +0900

    refactor: 음표 애니메이션 네이밍 수정

--- apps/web/src/features/pomodoro/components/NoteAnimation.tsx
+++ apps/web/src/features/pomodoro/components/NoteAnimation.tsx
@@ -2,8 +2,8 @@ import Note from '@/assets/note.svg?react';
 
 export function NoteAnimation() {
   return (
-    <div className="absolute -top-16 left-1/2 h-[78px] w-[128px] -translate-x-1/2 overflow-hidden">
-      <Note className="note-animation" />
+    <div className="absolute -top-16 left-1/2 h-19.5 w-32 -translate-x-1/2 overflow-hidden">
+      <Note className="animate-note-slide" />
     </div>
   );
 }
