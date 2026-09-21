191b3a1658afc8aeed78c631bfe07da145905a0e
young999999999 <ssas146310@gmail.com>
2026-09-22 00:02:48 +0900

    feat: [리뷰반영] 500번대 예외에 대해 에러 로그 출력

--- apps/api/src/common/filter/http-exception-logger.filter.ts
+++ apps/api/src/common/filter/http-exception-logger.filter.ts
@@ -8,10 +8,12 @@ export class HttpExceptionLoggerFilter extends BaseExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost): void {
     const status = exception.getStatus();
 
-    // 400번대 예외에 대해 로깅
+    const logMessage = `${exception.message}\n${exception.stack}`;
+
     if (status >= 400 && status < 500) {
-      const logMessage = `${exception.message}\n${exception.stack}`;
       this.logger.warn(logMessage);
+    } else if (status >= 500) {
+      this.logger.error(logMessage);
     }
 
     // BaseExceptionFilter가 예외 처리
